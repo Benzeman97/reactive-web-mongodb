c3f34448f90f26d8596f956c7735eae52647115b
Benz <Benzeman097-nafazbenzema@gmail.com>
2020-09-05 13:35:28 +0530

    unit test has been done

--- src/main/java/com/benz/reactive/web/ReactiveWebMongodbApplication.java
+++ src/main/java/com/benz/reactive/web/ReactiveWebMongodbApplication.java
@@ -1,5 +1,6 @@
 package com.benz.reactive.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReactiveWebMongodbApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ReactiveWebMongodbApplication.class, args);
     }
 
--- src/test/java/com/benz/reactive/web/test/FluxAndMonoTest.java
+++ src/test/java/com/benz/reactive/web/test/FluxAndMonoTest.java
@@ -1,7 +1,10 @@
 package com.benz.reactive.web.test;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 public class FluxAndMonoTest {
 
@@ -16,5 +19,56 @@ public class FluxAndMonoTest {
        stringFlux.subscribe(System.out::println
        ,ex->System.err.println("Exception is "+ex),
                ()->System.out.println("Completed"));
+    }
+
+
+    @Test
+    public void fluxTest_WithoutError()
+    {
+        Flux<String> stringFlux=Flux.just("Spring","Spring Boot","Reactive Programming")
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("Spring")
+                .expectNext("Spring Boot")
+                .expectNext("Reactive Programming")
+               .verifyComplete();
+
+    }
+
+    @Test
+    public void fluxTest_WithError()
+    {
+        Flux<String> stringFlux=Flux.just("Spring","Spring Boot","Reactive Programming")
+                .concatWith(Flux.error(()->new RuntimeException("Runtime Exception is occurred")))
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("Spring","Spring Boot","Reactive Programming")
+               //.expectError(RuntimeException.class)
+                .expectErrorMessage("Runtime Exception is occurred")
+                .verify();
+
+    }
+
+    @Test
+    public void monoTest()
+    {
+        Mono<String> stringMono=Mono.just("Spring Boot");
+
+        StepVerifier.create(stringMono.log())
+                .expectNext("Spring Boot")
+                .verifyComplete();
+    }
+
+    @Test
+    public void monoTest_WithError()
+    {
+
+
+        StepVerifier.create(Mono.error(()->new RuntimeException("Exception is occurred")).log())
+                //.expectError(RuntimeException.class)
+                .expectErrorMessage("Exception is occurred")
+                .verify();
     }
 }
